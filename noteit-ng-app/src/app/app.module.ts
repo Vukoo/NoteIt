c9826b5d65b57951faf0c3733af98d6b59be4b3c
Vukoo <piotrgadek4@gmail.com>
2020-09-26 17:12:06 +0200

    init commit

---
+++ noteit-ng-app/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NotesComponent } from './notes/notes.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes : Routes = [
+  {
+    path: 'notes',
+    component:NotesComponent
+    },
+  {
+    path: 'feedback',
+    component:FeedbackComponent
+   },
+   {
+   path: '',
+   component: NotesComponent,
+   pathMatch:'full'
+   },
+   {
+   path: '**',
+   component: NotFoundComponent
+   }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotFoundComponent,
+    NotesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
